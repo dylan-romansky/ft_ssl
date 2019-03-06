87a5eef996f59de6beeee9826480f8a1d7ca5191
Dylan Romansky <dromansk@e1z2r7p24.42.us.org>
2019-03-05 23:09:37 -0800

    makin sure

--- src/preprocessing.c
+++ src/preprocessing.c
@@ -6,7 +6,7 @@
 /*   By: dromansk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 02:29:52 by dromansk          #+#    #+#             */
-/*   Updated: 2019/03/05 18:18:33 by dromansk         ###   ########.fr       */
+/*   Updated: 2019/03/05 22:50:45 by dromansk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,59 +36,10 @@ void			split_input_512(char *input, int len)
 	}
 }
 
-unsigned char	get_byte(int num)
-{
-	unsigned char	byte;
-	unsigned char	i;
-
-	i = 128;
-	byte = 0;
-	while (i)
-	{
-		if (i & num)
-			byte |= 1;
-		if (i != 1)
-			byte <<= 1;
-		i >>= 1;
-	}
-	return byte;
-}
-
-unsigned char	*get_bytes(int num)
-{
-	int				j;
-	int				k;
-	unsigned char	*bytes;
-
-	j = 0;
-	k = 0;
-	bytes = (unsigned char *)malloc(sizeof(unsigned char) * 8);
-	while (num)
-	{
-		bytes[j++] = get_byte(num);
-		num >>= 8;
-	}
-	while (j < 8)
-		bytes[j++] = bytes[k++];
-	return (bytes);
-}
-
-char			*append_len(char *fixed, int i, long num)
-{
-	unsigned char	*bytes;
-	int				j;
-
-	bytes = get_bytes(num);
-	j = 0;
-	while (j < 8)
-		fixed[i++] = bytes[j++];
-	free(bytes);
-	return (fixed);
-}
-
 int				padding(char *input)
 {
 	size_t			len;
+	size_t			flen;
 	size_t			i;
 	char			*fixed;
 
@@ -99,15 +50,16 @@ int				padding(char *input)
 	words->offset = 0;
 	len = ft_strlen(input);
 	i = len;
-	while ((i + 8) % 512 || (i + 8) < 512)
+	while ((i + 8) % 64 || (i + 8) < 64)
 		i++;
 	if (!(fixed = ft_strnew(i + 8)))
 		return (-1);
-	fixed = ft_strcpy(fixed, input);
-	fixed[len] = 127;
-	fixed = append_len(fixed, i, 4294967296 % len);
-	ft_print_n(fixed, i + 8);
+	fixed = ft_memcpy(fixed, input);
+	fixed[len] = 128; //potential problem area, confirm this works
+	flen = len * 8;
+	ft_memcpy(fixed + i + 1, &flen, 4);
 	split_input_512(input, i + 8);
+	return (0);
 }
 
 int				md5(char *input, size_t len, t_words *words)
