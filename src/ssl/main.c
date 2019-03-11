a2296559e08d3dd8355a79c268eccff5d81c3df4
bigdiggysquiggle <dromansk@NutHouse.localdomain>
2019-03-11 04:31:50 -0700

    added read function and framework for flags parse. need to figure out how to tell between standard input and file

--- src/ssl/main.c
+++ src/ssl/main.c
@@ -16,29 +16,87 @@ unsigned long	flip_end_512(unsigned long n)
 			((n & 0xff00) << 40) | (n << 56));
 }
 
-int				ssl_flags(char **av, int ac, int flags, int dis)
+int				get_input(int flags, char *input, char **file)
 {
+	int				len;
+	int				ret;
+	int				fd;
+	char			buf[BUFF_SIZE + 1];
+	char			*file;
+
+	if (flags & s)
+	{
+		*file = ft_strdup(input);
+		return (ft_strlen(input));
+	}
+	file = ft_strnew(o);
+	fd = open_file(input);
+	len = 0;
+	if (fd == -1)
+		return (0);
+	while ((ret = read(fd, file, BUFF_SIZE)) > 0)
+	{
+		file[ret] = '\0';
+		file = swap_n_free(ft_strjoin(file, buf), &file);
+		len += ret;
+	}
+	if (ret < 0)
+	{
+		free(file);
+		return (0);
+	}
+	return (len);
+}
+
+void			do_ssl(int flags, char *input, int dis)
+{
+	char			*file;
+	int				len;
+
+	len = get_input(flags, input, &file);
+	if (flags & p)
+		ft_printf("%s\n", input);
 	if (!(flags & q))
 	{
 		if (flags & r)
+		{
+		}
 		else
 		{
-			ft_printf("%s (%s)", av[1], av[ac - 1]);
-			g_sslfuns[dis].hash(input, size);
 		}
+	}
+	else
 }
 
-void			do_ssl(char **av, int ac, int flags, int dis)
+void			ssl_flags(char **av, int ac)
 {
 	unsigned char	flags;
 	int				i;
+	int				j;
+	int				k = 0;
 
 	i = 0;
+	j = 0;
 	flags = 0;
-	while (g_sslflags[i].flag && av[2][1] != g_sslflags[i].flag)
-		i++;
-	flags += g_sslflags[i].value;
-
+	while (j < ac)
+	{
+		if (av[j][0] == '-')
+		{
+			k = 1;
+			if (av[j][k] == '-')
+				k++;
+			while (g_sslflags[i].flag && av[2][1] != g_sslflags[i].flag)
+				i++;
+			flags += g_sslflags[i].value;
+		}
+		else if (av[j][0] != '-')
+		{
+			do_ssl(flags, av[j]);
+			flags = 0; //verify that this resets
+		}
+		j++;
+	}
+	return (flags);
 }
 
 int				main(int ac, char **av)
