f8f37c398655296a59f4b6b8ef7b355e71ea46b1
Dylan Romansky <dromansk@e1z2r7p8.42.us.org>
2019-04-25 20:20:38 -0700

    victory

--- src/des/test.c
+++ src/des/test.c
@@ -6,7 +6,7 @@ int		main(void)
 	unsigned long	key = 0x133457799BBCDFF1;
 	unsigned long	*subkeys = gen_key(key);
 	unsigned long permuted = init_perm(message);
-	ft_printf("%llX\n", split_perm(permuted, subkeys));
+	ft_printf("result    %llX\n", split_perm(permuted, subkeys));
 	ft_printf("expecting 85E813540F0AB405\n");
 	return (0);
 }
