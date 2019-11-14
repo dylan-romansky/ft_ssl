2c6a45b21d1295e463ecdb28ad87c5cf19c96f41
Dylan Romansky <dromansk@e1z2r3p26.42.us.org>
2019-11-14 00:47:00 -0800

    works and normed

--- src/ssl/more_errors.c
+++ src/ssl/more_errors.c
@@ -6,7 +6,7 @@
 /*   By: dromansk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 17:07:09 by dromansk          #+#    #+#             */
-/*   Updated: 2019/05/09 17:08:32 by dromansk         ###   ########.fr       */
+/*   Updated: 2019/11/14 00:46:20 by dromansk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 void	des_len_error(int dis)
 {
 	if (dis == 6)
-		ft_printf("Error: des: Message not multiple of block length.");
+		ft_printf("Error: des: Message not multiple of block length.\n");
 	if (dis == 7)
-		ft_printf("Error: des-cbc: Message not multiple of block length.");
+		ft_printf("Error: des-cbc: Message not multiple of block length.\n");
 	if (dis == 8)
-		ft_printf("Error: des-ecb: Message not multiple of block length.");
+		ft_printf("Error: des-ecb: Message not multiple of block length.\n");
 	exit(1);
 }
