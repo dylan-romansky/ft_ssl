fb6c042606d4a8984a228561d4761641d7e4fb34
bigdiggysquiggle <dylanromansky@yahoo.com>
2020-05-17 23:43:12 -0700

    removed clutter

--- src/ssl/helpers.c
+++ src/ssl/helpers.c
@@ -26,18 +26,6 @@ unsigned long	flip_end_512(unsigned long n)
 			((n & 0xff000000) << 8) | ((n & 0xff0000) << 24) |
 			((n & 0xff00) << 40) | (n << 56));
 }
-/*does this even get used?
-int				stdin_check(void)
-{
-	char			buf[80];
-	int				number;
-
-	number = 0;
-	scanf("%79s", buf);
-	if (sscanf(buf, "%d", &number) == 1)
-		return (1);
-	return (0);
-}*/
 
 int				handle_string(char **av, int j, t_ssl_input *input, int dis)
 {
@@ -61,39 +49,8 @@ int				handle_string(char **av, int j, t_ssl_input *input, int dis)
 		input->sstring = av[j + 1];
 	}
 // verify how other ssl projects handle an -s with no string
-// for bufferization with string input, consider copying into
-// the buffer BUFF_SIZE at a time and running it through
 	do_ssl(input, NULL, dis);
 	input->sflag = NULL;
 	input->sstring = NULL;
 	return (ret);
 }
-
-/*
-int				get_input(int fd, char *input, char **file)
-{
-	int				len;
-	int				ret;
-	char			buf[BUFF_SIZE + 1];
-	char			*content;
-
-	if (flags & 8 && fd != 0)
-	{
-		*file = ft_strdup(input);
-		return (ft_strlen(input));
-	}
-	len = 0;
-	if (fd == -1)
-		return (0);
-	content = ft_strnew(0);
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
-	{
-		buf[ret] = '\0';
-		content = ft_hardjoin(content, len, buf, ret);
-		len += ret;
-	}
-	if (ret < 0)
-		free(content);
-	*file = content;
-	return (len);
-}*/
